b7bfef50a8a3c1b3610546de9b7dc3d80b3e7ee2
ryanhess <hess29@gmail.com>
2026-03-22 00:44:17 -0400

    Now uses transaction data ~8000 rows to train and test the data - 80% training, 20% test - clean the data and represent it numerically for XGBoost

--- src/cat-model.py
+++ src/cat-model.py
@@ -1,16 +1,41 @@
+import pandas as pd
 import xgboost
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
+from sklearn.preprocessing import LabelEncoder
 
-# Load data
-X, y = load_iris(return_X_y=True)
+
+def clean_data(data):
+    data.loc[data["Payee"].str.startswith("Transfer :"), "Category Group/Category"] = (
+        "Transfer"
+    )
+
+    payee_encoder = LabelEncoder()
+    data["Payee"] = payee_encoder.fit_transform(data["Payee"])
+    data["Outflow"] = data["Outflow"].replace(r"[\$,]", "", regex=True).astype(float)
+    data["Inflow"] = data["Inflow"].replace(r"[\$,]", "", regex=True).astype(float)
+
+    features = data[["Payee", "Outflow", "Inflow"]]
+
+    # label: Category Group/Category as ints
+    label_encoder = LabelEncoder()
+    labels = label_encoder.fit_transform(data["Category Group/Category"])
+
+    return features, labels
+
+
+# get the data from the ynab file
+raw = pd.read_csv("test_data/rh-ynab_test_data.csv")
+
+data, labels = clean_data(raw)
 
 # Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+traindata, testdata, trainlabels, testlabels = train_test_split(
+    data, labels, test_size=0.2
+)
 
 # Train
 model = xgboost.XGBClassifier()
-model.fit(X_train, y_train)
+model.fit(traindata, trainlabels)
 
 # Evaluate
-print(model.score(X_test, y_test))
+print(model.score(testdata, testlabels))
