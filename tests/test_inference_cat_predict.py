ad1da83588f59a3206cbcec77a85071f2c00541b
ryanhess <hess29@gmail.com>
2026-03-27 17:55:36 -0400

    WIP--reset

--- tests/test_inference_cat_predict.py
+++ tests/test_inference_cat_predict.py
@@ -1,6 +1,13 @@
+from transaction_categorizer.inference.cat.predict import (
+    predict,
+    _get_features_from_data,
+)
+
+
 class TestGetFeaturesFromData:
     pass
 
 
 class TestPredict:
-    pass
+    def test_returns_none_when_no_model(self) -> None:
+        return
