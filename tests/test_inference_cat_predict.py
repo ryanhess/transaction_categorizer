d78a4a7b64fd74b035a3a14cc4ce4f8d7ef3294f
ryanhess <hess29@gmail.com>
2026-03-27 20:01:03 -0400

    Predict: - Returns none when model not trained - Returns list of same length as input, of correct type objs.     -Skips the test and records reason if the model is in fact not trained. - raises ValueError from TfidTransformer when passed an empty array. - Categories are not treatable as ints--either ints or "1".

--- tests/test_inference_cat_predict.py
+++ tests/test_inference_cat_predict.py
@@ -1,7 +1,15 @@
+from pytest import mark, raises
 from transaction_categorizer.inference.cat.predict import (
     predict,
-    _get_features_from_data,
+    MODEL_IS_TRAINED,
 )
+from transaction_categorizer.models import TransactionRequest, TransactionResponse
+
+
+txns_param = [
+    TransactionRequest(id=1, payee="The Home Depot", inflow=0, outflow=10.25),
+    TransactionRequest(id=2, payee="Whole Foods", inflow=0, outflow=65.43),
+]
 
 
 class TestGetFeaturesFromData:
@@ -9,8 +17,31 @@ class TestGetFeaturesFromData:
 
 
 class TestPredict:
-    def test_returns_none_when_no_model(self) -> None:
-        return
+    def test_returns_none_when_no_model(self, mocker) -> None:
+        mocker.patch(
+            "transaction_categorizer.inference.cat.predict.MODEL_IS_TRAINED", new=False
+        )
 
-    def test_returns_list_of_correct_type(self) -> None:
-        return
+        result = predict(txns_param)
+
+        assert result == None
+
+    @mark.skipif(not MODEL_IS_TRAINED, reason="model not trained")
+    def test_returns_list_of_correct_type_and_count(self) -> None:
+        result = predict(txns_param)
+
+        assert isinstance(result, list)
+        assert len(result) == len(txns_param)
+        assert all(isinstance(res, TransactionResponse) for res in result)
+
+    @mark.skipif(not MODEL_IS_TRAINED, reason="model not trained")
+    def test_predict_raises_value_error_with_empty_list(self) -> None:
+        with raises(ValueError, match="TfidfTransformer"):
+            predict([])
+
+    @mark.skipif(not MODEL_IS_TRAINED, reason="model not trained")
+    def test_categories_in_result_are_strings(self) -> None:
+        result = predict(txns_param)
+
+        assert isinstance(result, list)
+        assert all(not res.category.isnumeric() for res in result)
