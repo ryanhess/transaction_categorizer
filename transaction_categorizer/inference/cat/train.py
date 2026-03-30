691ed4e125a3bca21a2cf53a655fb3b2f7ec6e89
ryanhess <hess29@gmail.com>
2026-03-30 14:29:30 -0400

    update the strings in paths.py to be Paths

--- transaction_categorizer/inference/cat/train.py
+++ transaction_categorizer/inference/cat/train.py
@@ -74,38 +74,6 @@ def _store_model(
     dump(label_encoder, label_encoder_filepath)
 
 
-def _clean_data_and_get_transformers(
-    data,
-) -> tuple[coo_matrix, ArrayLike, TfidfVectorizer, LabelEncoder]:
-    # find rows where the payee starts with transfer, put Transfer in the category.
-    data.loc[data["Payee"].str.startswith("Transfer :"), "Category Group/Category"] = (
-        "Transfer"
-    )
-
-    data["Payee"] = data["Payee"].fillna("")
-    data["Category Group/Category"] = data["Category Group/Category"].fillna(
-        "Uncategorized"
-    )
-    counts = data["Category Group/Category"].value_counts()
-    keep = counts[counts >= 2].index
-    data = data[data["Category Group/Category"].isin(keep)]
-
-    payee_vectorizer = TfidfVectorizer()
-    payee_features = payee_vectorizer.fit_transform(data["Payee"])
-    data["Outflow"] = data["Outflow"].replace(r"[\$,]", "", regex=True).astype(float)
-    data["Inflow"] = data["Inflow"].replace(r"[\$,]", "", regex=True).astype(float)
-
-    money_features = data[["Outflow", "Inflow"]].values
-
-    features = hstack([payee_features, money_features])
-    features_matrix = cast(coo_matrix, features)
-
-    label_encoder = LabelEncoder()
-    labels = label_encoder.fit_transform(data["Category Group/Category"])
-
-    return features_matrix, labels, payee_vectorizer, label_encoder
-
-
 def _transform_data(
     data: pd.DataFrame, transformers: tuple
 ) -> tuple[coo_matrix, ArrayLike]:
