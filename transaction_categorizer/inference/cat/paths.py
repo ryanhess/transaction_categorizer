c89510f8cd7f5d41f4178650e42106bb8a9d11c4
ryanhess <hess29@gmail.com>
2026-03-30 09:17:11 -0400

    adjust

--- transaction_categorizer/inference/cat/paths.py
+++ transaction_categorizer/inference/cat/paths.py
@@ -3,7 +3,7 @@ _path_to_model_state = "transaction_categorizer/inference/cat/state/"
 training_data_filepath = (
     "transaction_categorizer/inference/cat/training_data/ynab-rh-txns.csv"
 )
-hyperparams_filepath = _path_to_model_state + "hyperparams.json"
+training_params_filepath = _path_to_model_state + "training_params.json"
 model_filepath = _path_to_model_state + "model.json"
 payee_vectorizer_filepath = _path_to_model_state + "payee_vectorizer.pkl"
 label_encoder_filepath = _path_to_model_state + "category_encoder.pkl"
